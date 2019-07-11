05de38a3a2308402ec5820dadffb12fa3be1f9d6
stu-40 <>
2019-07-11 16:28:07 +0800

    第二张弹出键盘输入设置

---
+++ Class03/AsyncReponseDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AsyncReponseDelegate.swift
+//  Class03
+//
+//  Created by stu-40 on 2019/7/11.
+//  Copyright © 2019 苹果工坊. All rights reserved.
+//
+
+import Foundation
---
+++ Class03/LoginFlow/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Class03
+//
+//  Created by stu-40 on 2019/7/11.
+//  Copyright © 2019 苹果工坊. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
